65d9cdbf2a39cce642884892e9bf244549555b19
Eyder Rios <eyder.rios@gmail.com>
2023-09-28 18:04:10 -0300

    Aula 2023-09-28

---
+++ number/bin/main.dart
@@ -0,0 +1,20 @@
+// ignore: avoid_relative_lib_imports
+import '../lib/number.dart';
+
+void main() {
+  var n1 = const Number(100);
+  var n2 = const Number(-10);
+
+  print('n1: $n1\t(${n1.hashCode})');
+  print('n2: $n2\t(${n2.hashCode})');
+
+  print('n1 is zero\t: ${n1.isZero()}');
+  print('n1 is positive\t: ${n1.isPositive()}');
+  print('n1 is negative\t: ${n1.isNegative()}');
+  print('n1 is even\t: ${n1.isEven()}');
+  print('n1 is odd\t: ${n1.isOdd()}');
+  print('n1 is prime\t: ${n1.isPrime()}');
+  print('n1 is Armstrong\t: ${n1.isArmstrong()}');
+  // print('n1!\t\t: ${n1.factorial()}');
+  print('n1 reversed: ${n1.reverse()}');
+}
---
+++ number/lib/number.dart
@@ -0,0 +1,89 @@
+import 'dart:math' as math;
+
+class Number {
+  final int _number;
+
+  //Number(this._number);
+  const Number(int number) : _number = number;
+
+  @override
+  String toString() {
+    return '$_number';
+  }
+
+  bool isZero() => _number == 0;
+  bool isPositive() => _number > 0;
+  bool isNegative() => _number < 0;
+  bool isEven() => _number % 2 == 0;
+  bool isOdd() => _number % 2 != 0;
+  int sqr() => _number * _number;
+  double sqrt() => math.sqrt(_number);
+  String toRadix(int radix) => _number.toRadixString(radix);
+
+  bool isPrime() {
+    if (_number < 2) {
+      return false;
+    }
+    for (int d = 2; d < _number; d++) {
+      if (_number % d == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  // Examples: 0, 1, 153, 370, 371, 407
+  bool isArmstrong() {
+    int number = _number;
+    int digits = number.toString().length;
+    int sum = 0;
+
+    if (number < 0) {
+      return false;
+    }
+
+    while (number != 0) {
+      var d = number % 10;
+      var p = math.pow(d, digits).toInt();
+
+      sum = sum + p;
+
+      number = number ~/ 10;
+    }
+
+    return sum == _number;
+  }
+
+  int factorial() {
+    int mulBinaryDigits(int a, int b) {
+      return math.log(a) ~/ math.log(b) + 1;
+    }
+
+    int f = 1;
+
+    for (int k = 2; k <= _number; k++) {
+      if (mulBinaryDigits(f, k) > 63) {
+        throw Exception('Overflow: result is to big.');
+      }
+      f = f * k;
+    }
+
+    return f;
+  }
+
+  int digitsSum([int radix = 10]) {
+    int number = _number.abs();
+    int sum = 0;
+
+    while (number != 0) {
+      sum += number % radix;
+      number = number ~/ radix;
+    }
+    return sum;
+  }
+
+  int reverse() {
+    var number = _number.toString().split('').reversed.join();
+    return int.parse(number);
+  }
+}
